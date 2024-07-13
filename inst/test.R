48dd4b313a60c0847c6a32d0f1e335781ea8521b
Alejandro Verri <averri@gmail.com>
2024-07-13 20:07:33 -0300

    import R20

--- inst/test.R
+++ inst/test.R
@@ -1,7 +1,21 @@
-devtools::load_all()
+# Example #1
 # GMDP <- buildGMDP(path="~/Database/gmdp/output/classical/BC/ARMA611",IDo="ARMA619",Vref=760,Vs30_STEP=10,ITo=1)
-GMDP <- buildGMDP(path="~/Database/gmdp/output/classical/BC/ARMA611",IDo="ARMA619",Vref=760,Vs30_STEP=10,ITo=1)
+devtools::load_all()
+GMDP <- buildGMDP(Vref=250,path="/Users/averri/Database/openquake/output/ARMV4D/gem250/",IDo="ARM4V4D")
 
 
+
+
+# Example #2
+devtools::load_all()
+GMDP <- buildGMDP(Vref=760,Vs30=c(450,290),path="/Users/averri/Database/openquake/output/ARMV4D/test760/",IDo="ARM4V4D")
+
+# Example #3
+S1 <- seq(SIDtoVs30("E"), SIDtoVs30("A"), by = Vs30_STEP)
+S2 <- sapply(c("B","BC", "C", "CD", "D", "DE", "E"), SIDtoVs30) |> unname()
+Vs30_TARGET <- c(S1,S2,Vref) |> unique() |> sort()
+
+
+# Example #4
 devtools::load_all()
 buildTable.Kmax(.x=GMDP$UHSTable,Vs30o=c(200,300,400),Tso=0.12,Dao=15.0,po=0.50,TRo=c(500,5000,10000))
